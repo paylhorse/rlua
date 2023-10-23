c398b71a351d6ae85d244d91f7ae8a1c307662e3
paylhorse <realalsuwaidi@gmail.com>
2023-10-23 04:52:16 -0700

    Statically link LuaJIT and make default

--- build.rs
+++ build.rs
@@ -1,51 +1,14 @@
 fn main() {
-    let mut lua_version_features = 0;
-    #[cfg(feature = "builtin-lua54")]
-    {
-        println!("cargo:rustc-cfg=rlua_lua54");
-        lua_version_features += 1;
-    }
+    // Specify that you're using Lua 5.1 with LuaJIT
+    println!("cargo:rustc-cfg=rlua_lua51");
+    println!("cargo:rustc-cfg=rlua_luajit");
 
-    #[cfg(feature = "builtin-lua53")]
-    {
-        println!("cargo:rustc-cfg=rlua_lua53");
-        lua_version_features += 1;
-    }
+    // Specify the path to the static library within the rlua repository
+    let luajit_lib_path = "libs";
 
-    #[cfg(feature = "builtin-lua51")]
-    {
-        println!("cargo:rustc-cfg=rlua_lua51");
-        lua_version_features += 1;
-    }
+    // Add it to the linker search path
+    println!("cargo:rustc-link-search=native={}", luajit_lib_path);
 
-    #[cfg(feature = "system-lua54")]
-    {
-        println!("cargo:rustc-cfg=rlua_lua54");
-        lua_version_features += 1;
-    }
-
-    #[cfg(feature = "system-lua53")]
-    {
-        println!("cargo:rustc-cfg=rlua_lua53");
-        lua_version_features += 1;
-    }
-
-    #[cfg(feature = "system-lua51")]
-    {
-        println!("cargo:rustc-cfg=rlua_lua51");
-        lua_version_features += 1;
-    }
-
-    #[cfg(feature = "system-luajit")]
-    {
-        println!("cargo:rustc-cfg=rlua_lua51");
-        println!("cargo:rustc-cfg=rlua_luajit");
-        lua_version_features += 1;
-    }
-
-    if lua_version_features < 1 {
-        panic!("No Lua version specified.  Please enable one of the features. use --no-default-features to disable default lua feature.");
-    } else if lua_version_features > 1 {
-        panic!("Cannot enable more than one Lua interpreter feature.");
-    }
+    // Specify the library to link against
+    println!("cargo:rustc-link-lib=static=luajit");
 }
